051dc8e100f2762239e1d7b55483a17de2e79351
sompal <sompal.kumar@qwertythoughts.com>
2022-12-21 13:08:24 +0530

    new commit

--- src/components/UI/modal/Modal.js
+++ src/components/UI/modal/Modal.js
@@ -4,10 +4,13 @@ function Modal(props) {
     <>
       <div className={classes.container}>
         <div className={classes.modal}>
-          <h3 className={classes.heading}>Are you sure, want to delete</h3>
-          <span className={classes.close} onClick={props.onCancel}>
+          <p className={classes.close} onClick={props.onCancel}>
             close
-          </span>
+          </p>
+          <h3 className={classes.heading}>Are you sure?</h3>
+          <p className={classes.para}>
+            Are you sure sure? You want to delete this meetup item.
+          </p>
           <div className={classes.buttonBox}>
             <button className={classes.delete} onClick={props.onClick}>
               Delete
